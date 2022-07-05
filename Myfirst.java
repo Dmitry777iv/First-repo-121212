8128ea408357a0a6f7d2a7557812d4aafe1e897c
Dima777iv <108660517+Dmitry777iv@users.noreply.github.com>
2022-07-05 23:06:02 +0300

    Add files via upload

---
+++ Myfirst.java
@@ -0,0 +1,8 @@
+public class Myfirst {
+    public static void main(String[] args) {
+        int x = 228;
+        byte c = -33;
+        final int AGE = 5;
+        System.out.println(x+c+AGE);
+    }
+}
