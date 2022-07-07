9f7f09ef550f86ebcb3bcfa68c72b3a3c725e644
Dima777iv <108660517+Dmitry777iv@users.noreply.github.com>
2022-07-07 18:03:12 +0300

    Add files via upload

---
+++ LabRab4IvanovDV.java
@@ -0,0 +1,8 @@
+public class LabRab4IvanovDV {
+
+    public static void main (String[] args){
+        for (int i=1; i<10; i++) {
+        int j = 7;
+        System.out.println(i+"*"+j+"="+i*j);}
+    }
+}
